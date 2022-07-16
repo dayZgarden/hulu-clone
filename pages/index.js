4dc1f94d96b3128d8954c03abf1d4ad3b9a6ec15
dayZgarden <zyad.alkurdi22@gmail.com>
2022-07-16 19:15:14 -0400

    Added icon

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
+import '../public/favicon.ico'
 import requests from '../utils/requests'
 
 export default function Home( {results} ) {
